833a75dcb4660d96343e960838f7daf87a7f9eaa
Ray Samuels <ray@samuels.io>
2020-06-07 08:18:43 +0100

    Updates

---
+++ precache-manifest.69afc55276d2a75b8548c5feef331fe2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b15598d250d3f776f90644f888ecfd",
+    "url": "/popcorn-picker/index.html"
+  },
+  {
+    "revision": "209d98d700906b9e1fd6",
+    "url": "/popcorn-picker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "abe1cf6acad99da1bec3",
+    "url": "/popcorn-picker/static/css/main.71f522e1.chunk.css"
+  },
+  {
+    "revision": "209d98d700906b9e1fd6",
+    "url": "/popcorn-picker/static/js/2.4716764d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/popcorn-picker/static/js/2.4716764d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe1cf6acad99da1bec3",
+    "url": "/popcorn-picker/static/js/main.cc17a0e6.chunk.js"
+  },
+  {
+    "revision": "0d544206e3c40f24ce1e",
+    "url": "/popcorn-picker/static/js/runtime-main.57b3edf3.js"
+  },
+  {
+    "revision": "842800a6b923791c0592232cd0384e9a",
+    "url": "/popcorn-picker/static/media/bg.842800a6.jpg"
+  },
+  {
+    "revision": "c118864522cd2b21f517965b506a7b7d",
+    "url": "/popcorn-picker/static/media/popcorn-picker.c1188645.png"
+  }
+]);
